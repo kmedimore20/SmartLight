e711d998f02c5769b12668fbae9e5e324c87cb22
Karlo Međimorec <100922130+kmedimore20@users.noreply.github.com>
2022-12-12 19:43:37 +0100

    Add files via upload

--- JavaScript/mqtt_publish.js
+++ JavaScript/mqtt_publish.js
@@ -1,7 +1,7 @@
 var mqtt;
 var reconnectTimeout = 2000;
-var host = "test.mosquitto.org";
-var port = 8080;
+var host = "broker.hivemq.com";
+var port = 8000;
 
             function onConnect() {
                 console.log("Connected");
